798704d39a55e8e47760172369c0261e2b814e65
Igor Oparin <igorop3@gmail.com>
2023-04-07 00:30:41 +0800

    init

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Exercise from "./pages/exercise";
 import Main from "./pages/main/index";
 import LoginPage from "./pages/loginPage";
-import SelectWorkout from "./components/selectWorkout/SelectWorkout";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<SelectWorkout />} />
+      <Route path="/" element={<Main />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registration" element={"<Registration />"} />
       <Route path="/description" element={"<Description />"} />
