5fd13ed7da8f46ebd81010410decb98f30b06531
Igor Oparin <igorop3@gmail.com>
2023-04-18 20:42:50 +0800

    init

--- src/components/selectWorkout/SelectWorkoutStyle.js
+++ src/components/selectWorkout/SelectWorkoutStyle.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
 export const Content = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 444px;
+  max-height: 626px;
+  border-radius: 12px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 36px 41px 58px;
+  background: #fff;
+  box-sizing: border-box;
 `;
 
 export const Title = styled.p`
